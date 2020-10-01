2178f10667dbd9c766063f38ad922975890a125c
Shubham Raj <54909290+Shubhamraj0106@users.noreply.github.com>
2020-10-01 20:05:13 +0530

    Update animations.dart

--- lib/animations.dart
+++ lib/animations.dart
@@ -1,5 +1,7 @@
 /// Animation tools that are not provided by the Flutter SDK
-library animations;
+Hero(
+  child:Image.asset('Image/image_name.jpg),
+ ) 
 
 export 'src/radial_drag_gesture_detector.dart';
 export 'src/animation_player.dart';
